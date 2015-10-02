52dec1512380a42b0dcf3c6dad20877f3338f107
RodneyLovely <jacob.keszei@gmail.com>
2015-10-02 15:04:39 -0400

    Exercise 3 taken to the engine starting then dying.

--- exercise3.py
+++ exercise3.py
@@ -39,6 +39,24 @@ Is the car silent when you turn the key? """)
         else:
             print("ERROR: Press 'y' or 'n' in order for us to guide you. Please start again.")
 
+#car click branches
+    if key_answer == "n":
+        click_answer = raw_input("Does the car make a clicking noise? ")
+        if click_answer == "y":
+            print("Replace the battery.")
+        elif click_answer == "n":
+            fail_answer = raw_input("Does the car crank up but fail to start? ")
+        else:
+            print("ERROR: Press 'y' or 'n' in order for us to guide you. Please start again.")
+
+#fail to start branches
+        if fail_answer == "y":
+            print("Check spark plug connections.")
+        elif fail_answer == "n":
+            start_answer = raw_input("Does the engine start then die? ")
+        else:
+            print("ERROR: Press 'y' or 'n' in order for us to guide you. Please start again.")
+
 
 
 #error message at start
