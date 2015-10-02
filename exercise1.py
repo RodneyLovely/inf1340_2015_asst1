187d3d7411fca582ccd64965138d9996a3474cf3
RodneyLovely <jacob.keszei@gmail.com>
2015-10-02 12:30:37 -0400

    I've cut the number of lines, and condensed the broker_commission into one variable, since it'll automatically stand for something else if we change the value.
    Also, for the readability of the program, I've reduced the words in the variables.
    
    Finally, I created a boolean at the end, due to exercise 1's "if" statements.

--- exercise1.py
+++ exercise1.py
@@ -13,39 +13,18 @@ __email__ = "ses@drsusansim.org"
 __copyright__ = "2015 Susan Sim"
 __license__ = "MIT License"
 
-# Calculate amount spent on stock
-# Calculate commission paid to broker for purchase
-# Calculate amount made on sale of stock
-# Calculate commission paid to broker for sale
-# Calculate difference between money made and spent
 
-number_of_shares = 2000
-unit_price_purchase = 900.00
-commission_on_sale = .03
-purchase_price = number_of_shares * unit_price_purchase
-amount_paid_to_broker_for_purchase = purchase_price * commission_on_sale
+shares = 2000
+share_cost = 900.00
+broker_commission = (shares * share_cost) * 0.03
 
-print(number_of_shares * unit_price_purchase)
-print(purchase_price * commission_on_sale)
-print(purchase_price + amount_paid_to_broker_for_purchase)
+#Two weeks later...
 
-unit_price_sale = 942.75
-sale_price = (unit_price_sale * number_of_shares)
-commission_paid_to_broker_for_sale = sale_price * commission_on_sale
-money_made_on_sale = sale_price - commission_paid_to_broker_for_sale
+share_sale = 942.75
+broker_commission = (shares * share_sale) * 0.03 + broker_commission
+Lakshmi_total = (shares * share_sale - shares * share_cost) - broker_commission
 
-print(sale_price)
-print(commission_paid_to_broker_for_sale)
-print(money_made_on_sale)
-
-net_profit = money_made_on_sale - (purchase_price + amount_paid_to_broker_for_purchase)
-print(net_profit)
-
-money = net_profit
-print(money)
-
-
-# this is just a rough of the code
-# we would only need to have the final print function for what we hand in
-# I removed all of the extra print functions in pythontutor.com and it works
-#also checks out on paper
+if (Lakshmi_total < 0):
+    print("Lakshmi lost money, at a total of: " + str(Lakshmi_total) + " dollars.")
+else:
+    print("Lakshmi made a profit, at a total of: " + str(Lakshmi_total) + " dollars.")
