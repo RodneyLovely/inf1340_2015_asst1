c1edc8f6afb2ecd16175e717790b120b79e49a96
aacampb <aaronl.campbell@mail.utoronto.ca>
2015-10-02 21:50:48 -0400

    Final Revision

--- exercise1.py
+++ exercise1.py
@@ -7,14 +7,25 @@ after the stock transactions
 
 """
 
+#Calculate the amount Lakshmi spent on the share purchase, including the commission paid to her broker
+#Calculate the amount Lakshmi received on the share purchase, including the commission paid to her broker
+#Calculate the difference between the amount made on the sale with the amount spent on the purchase
+#output should be a positive or negative number
 
 shares = 2000
-shares_purchase_price = 900.00
+purchase_price = 900.00
 broker_commission = 0.03
-shares_sale_price = 942.75
+sale_price = 942.75
 
-shares_purchase = (shares * shares_purchase_price) + (shares * shares_purchase_price * broker_commission)
-shares_sale = (shares * shares_sale_price) - (shares * shares_sale_price * broker_commission)
-money = shares_sale - shares_purchase
+share_purchase = (shares * purchase_price) + (shares * purchase_price * broker_commission)
+shares_sale = (shares * sale_price) - (shares * sale_price * broker_commission)
+money = shares_sale - share_purchase
 
 print(money)
+
+#tested with:
+#shares = 100, 3000, 2000
+#purchase_price = 10.00, 175.00, 825.00
+#broker_commission = 0.05, 0.03, 0.03
+#sale_price = 15.00, 187.50, 862.90
+#output = 375.0, 4875.0, -25474.0
