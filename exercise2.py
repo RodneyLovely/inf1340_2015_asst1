264c212514c2945e168c60731560bba1b777089a
Aaron Campbell <aaronl.campbell@mail.utoronto.ca>
2015-10-08 12:20:08 -0400

    Updated exercise3

--- exercise2.py
+++ exercise2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-
 __author__ = 'Susan Sim'
 __email__ = "ses@drsusansim.org"
 __copyright__ = "2015 Susan Sim"
@@ -10,8 +9,11 @@ __license__ = "MIT License"
 def name_that_shape():
     """
     For a given number of sides in a regular polygon, returns the shape name
+
     Inputs: int 3, 4, 5, 6, 7, 8, 9, 10
+
     Expected Outputs: str Triangle, Quadrilateral, Pentagon, Hexagon, Heptagon, Octogon, Nonagon, Decagon
+
     Errors: any int < 3, or > 10
     """
     number_of_sides = raw_input("How many sides does your shape have? Enter a number between 3 and 10: ")
@@ -35,4 +37,4 @@ def name_that_shape():
     elif number_of_sides < "3" or number_of_sides > "10":
         print("Error")
 
-#name_that_shape()
+name_that_shape()
--- exercise3.py
+++ exercise3.py
@@ -13,69 +13,6 @@ __copyright__ = "2015 Susan Sim"
 __license__ = "MIT License"
 
 
-def diagnose_car():
-    """
-    Interactively queries the user with yes/no questions to identify a
-    possible issue with a car.
-
-    Inputs: only a raw_input of "y" or "n"
-
-    Expected Outputs: a progression that builds on the previous question, depending on the answer of "y" or "n"
-
-    Errors: anything entered other than a raw_input of "y" or "n" prints an error statement
-
-    """
-
-    error = ("ERROR")
-
-    key = raw_input("""Please press 'y' for "yes" and 'n' for "no."
-Is the car silent when you turn the key? """)
-
-    if key == "y":
-        battery = raw_input("Are the battery terminals corroded? ")
-        if battery == "y":
-            print("Clean terminals and try starting again.")
-        elif battery == "n":
-            print("Replace cables and try again.")
-        else:
-            print(error)
-
-    elif key == "n":
-        click = raw_input("Does the car make a clicking noise? ")
-        if click == "y":
-            print("Replace the battery.")
-        elif click == "n":
-            crank = raw_input("Does the car crank up but fail to start? ")
-            if crank == "y":
-                print("Check spark plug connections.")
-            elif crank == "n":
-                engine = raw_input("Does the engine start and then die? ")
-                if engine == "n":
-                    #I changed the 'print' statement due to the class email from Oct. 7
-                    print("Engine is not getting enough fuel. Clean fuel pump.")
-                elif engine == "y":
-                    fuel = raw_input("Does your car have fuel injection? ")
-                    if fuel == "n":
-                        print("Check to ensure the choke is opening and closing.")
-                    elif fuel == "y":
-                        print("Get it in for service.")
-                    else:
-                        print(error)
-                else:
-                    print(error)
-            else:
-                print(error)
-        else:
-            print(error)
-    else:
-        print(error)
-
-#I'm not sure if I did it in elegant fashion, but this works at every step
-#including an error message if anything but 'y' or 'n' is pressed at any stage
-#I think the program could probably be clarified by good identifiers and better readability perhaps
-
-
-diagnose_car()
 
 def diagnose_car():
     """
@@ -91,41 +28,29 @@ def diagnose_car():
     """
 
     diag1 = raw_input("Is the car silent when you turn the key?: ")
-    if diag1 == "y":
+    if diag1 == "Y":
         diag2 = raw_input("Are the battery terminals corroded?: ")
-        if diag2 == "y":
+        if diag2 == "Y":
             print("Clean terminals and try starting again.")
-        elif diag2 == "n":
+        elif diag2 == "N":
             print("Replace cables and try again.")
-        else:
-            print("Error")
-    elif diag1 == "n":
+    elif diag1 == "N":
         diag3 = raw_input("Does the car make a clicking noise?: ")
-        if diag3 == "y":
+        if diag3 == "Y":
             print("Replace the battery.")
-        elif diag3 == "n":
+        elif diag3 == "N":
             diag4 = raw_input("Does the car crank up but fail to start?: ")
-            if diag4 == "y":
+            if diag4 == "Y":
                 print("Check spark plug connections.")
-            elif diag4 == "n":
+            elif diag4 == "N":
                 diag5 = raw_input("Does the engine start then die?: ")
-                if diag5 == "y":
+                if diag5 == "Y":
                     diag6 = raw_input("Does your car have fuel injection?: ")
-                    if diag6 == "n":
+                    if diag6 == "N":
                         print("Check to ensure the choke is opening and closing.")
-                    elif diag6 == "y":
+                    elif diag6 == "Y":
                         print("Get it in for service.")
-                    else:
-                        print("Error")
-                elif diag5 == "n":
+                elif diag5 == "N":
                     print("Engine is not getting enough fuel. Clean fuel pump.")
-                else:
-                    print("Error")
-            else:
-                print("Error")
-        else:
-            print("Error")
-    else:
-        print("Error")
 
-#diagnose_car()
+diagnose_car()
